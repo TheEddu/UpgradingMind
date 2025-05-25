581e80786fc24e220044032504fb0e266a7368ac
TheEddu <edsan42@protonmail.com>
2025-05-24 21:09:30 -0300

    commit

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ document.getElementById('header').innerHTML = `
         UpgradingMind
       </h1>
       <nav class="space-x-6 font-mono text-lg">
-        <a href="index.html" class="px-4 py-2 rounded-md bg-[#1e293b] text-slate-100 hover:bg-[#334155] transition">Home</a>
+        <a href="/index.html" class="px-4 py-2 rounded-md bg-[#1e293b] text-slate-100 hover:bg-[#334155] transition">Home</a>
         <a href="https://theeddu.github.io/PersonalPortfolioPage/" target="_blank" class="px-4 py-2 rounded-md bg-[#1e293b] text-slate-100 hover:bg-[#334155] transition">Meu Portfolio</a>
         <a href="https://theeddu.github.io/Cyber-Blog/" target="_blank" class="px-4 py-2 rounded-md bg-[#1e293b] text-slate-100 hover:bg-[#334155] transition">Meus Artigos</a>
         <a href="https://theeddu.github.io/PortfolioLabs/" target="_blank" class="px-4 py-2 rounded-md bg-[#1e293b] text-slate-100 hover:bg-[#334155] transition">Portfolio de Labs</a>
